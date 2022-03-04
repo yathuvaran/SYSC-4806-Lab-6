9eecae6787b719896f2731d8dbc1c30bee92d7ff
yathuvaran <49103000+yathuvaran@users.noreply.github.com>
2022-03-04 00:08:54 -0500

    Update HttpRequestTest.java

--- src/test/java/addressBook/HttpRequestTest.java
+++ src/test/java/addressBook/HttpRequestTest.java
@@ -23,15 +23,15 @@ public class HttpRequestTest {
     public void greetingShouldReturnDefaultMessage() throws Exception {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/result",
                 String.class)).isEqualTo(
-                        "<!DOCTYPE HTML>\r\n" +
-                                "<html>\r\n" +
-                                "<head>\r\n" +
-                                "    <title>Submitted</title>\r\n" +
-                                "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>\r\n" +
-                                "</head>\r\n" +
-                                "<body>\r\n" +
-                                "<h1>Address Book Uploaded</h1>\r\n" +
-                                "</body>\r\n" +
+                        "<!DOCTYPE HTML>\n" +
+                                "<html>\n" +
+                                "<head>\n" +
+                                "    <title>Submitted</title>\n" +
+                                "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>\n" +
+                                "</head>\n" +
+                                "<body>\n" +
+                                "<h1>Address Book Uploaded</h1>\n" +
+                                "</body>\n" +
                                 "</html>"
         );
     }
